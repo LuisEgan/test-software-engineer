e61b17d7e419e40d5757e0a5f5fc941c8a335b6e
Luis Egan <luisdegan10@hotmail.com>
2018-01-03 15:38:22 -0300

    fix product not beign mutated

--- src/coTest.js
+++ src/coTest.js
@@ -24,32 +24,33 @@ class CarInsurance {
     const MC_PRICE = 80;
 
     products.forEach( product => {
-      let { name, price, sellIn } = product;
+      let { name } = product;
 
       const isFC = name === COVERAGES.FC;
       const isSFC = name === COVERAGES.SFC;
       const isMC = name === COVERAGES.MC;
       const isSS = name === COVERAGES.SS;
 
-      const atMaxPrice = price >= MAX_PRICE;
+      const atMaxPrice = product.price >= MAX_PRICE;
 
-      // If the item is not a Full coverage type
+      // If the product is not a Full coverage type
       if (!isFC && !isSFC) {
-        if (price > 0) {
+        console.log(name, 'here');
+        if (product.price > 0) {
           if (!isMC) {
-            price -= 1;
-            (isSS) && (price -= 1);
+            product.price -= 1;
+            (isSS) && (product.price -= 1);
           }
         }
       } else {
         if (!atMaxPrice) {
-          price += 1;
+          product.price += 1;
           if (isSFC) {
-            if (sellIn < 11) {
-              (!atMaxPrice) && (price += 1);
+            if (product.sellIn < 11) {
+              (!atMaxPrice) && (product.price += 1);
             }
-            if (sellIn < 6) {
-              (!atMaxPrice) && (price += 1);
+            if (product.sellIn < 6) {
+              (!atMaxPrice) && (product.price += 1);
             }
           }
         }
@@ -57,32 +58,34 @@ class CarInsurance {
 
       // Decrease sellIn for all products, except for the MC ones.
       if (!isMC) {
-        sellIn -= 1;
+        product.sellIn -= 1;
       }
 
-      if (sellIn < 0) {
+      if (product.sellIn < 0) {
         if (!isFC) {
           if (!isSFC) {
             // If the product is MC or SS
-            if (price > 0) {
+            if (product.price > 0) {
               if (!isMC) {
-                price -= 1;
-                (isSS) && (price -= 1);
+                product.price -= 1;
+                (isSS) && (product.price -= 1);
               }
             }
           } else {
-            price = 0;
+            product.price = 0;
           }
         } else {
-          (!atMaxPrice) && (price += 1);
+          (!atMaxPrice) && (product.price += 1);
         }
       }
 
       // Fallback for MC to ensure its price is always MC_PRICE
       if (isMC) {
-        price = MC_PRICE;
+        product.price = MC_PRICE;
       }
     });
+
+
 
     return products;
   }
